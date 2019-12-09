7a7369dc19483688521b3184d86de66234c970a7
dilanrinconhbtn <937@holbertonschool.com>
2019-12-09 21:07:13 +0000

    feature - adding comments

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { '/tmp/holberton':
-  path    => '/tmp/holberton',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+  path    => '/tmp/holberton', # path
+  mode    => '0744',         # permissions
+  owner   => 'www-data',       # owner
+  group   => 'www-data',       # group
+  content => 'I love Puppet',  # content
 }
