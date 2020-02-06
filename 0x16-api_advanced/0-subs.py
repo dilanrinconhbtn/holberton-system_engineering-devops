a81f1fc21ae6134e21cc1691a841c1786ac0b755
dilanrinconhbtn <937@holbertonschool.com>
2020-02-06 16:48:18 -0500

    fix return fail

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ number of subs"""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
+
     headers = {"User-Agent": "dilanrincon"}
     request = requests.get('https://api.reddit.com/r/{}/about'.
                             format(subreddit),
                             headers=headers)
+    if request.status_code == 404:
+        return 0
     subs = request.join()
     sub = subs['data']['subscribers']
     return sub
