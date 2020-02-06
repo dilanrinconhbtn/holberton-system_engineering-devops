6c07c5edd2a2b059f26a80f17b58fe2465fe02f2
dilanrinconhbtn <937@holbertonschool.com>
2020-02-06 21:58:31 +0000

    pep8 and executable

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,12 @@ def number_of_subscribers(subreddit):
     """ number of subs"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    try:
-        headers = {"User-Agent": "dilanrincon"}
-        request = requests.get('https://api.reddit.com/r/{}/about'.
-                                format(subreddit),
-                                headers=headers)
-        subs = request.join()
-        sub = subs['data']['subscribers']
-        return sub
-    except:
+    headers = {"User-Agent": "dilanrincon"}
+    request = requests.get('https://api.reddit.com/r/{}/about'.
+                           format(subreddit),
+                           headers=headers)
+    if subRinfo.status_code == 404:
         return 0
+    subs = request.join()
+    sub = subs['data']['subscribers']
+    return sub
